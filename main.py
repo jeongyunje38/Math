8131aa11f0f9efe04cf25b12f7089376d1ad56b0
YunjeJeong <jeongyunje38@gmail.com>
2021-07-15 13:55:19 +0900

    step fix

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import numpy as np
 
 
 def function(x):
-    # return 1 / (1 + np.exp(-x))
-    # return np.log2(x)
     # return x**2
-    return 2 * np.sin(x) + 4
+    # return 1 / (1 + np.exp(-x))
+    # return 2 * np.sin(x) + 4
+    # return np.cos(x) + np.exp(x)
+    return np.log2(x)
 
 
 def draw_rectangle(ax1, ax2, x1, x2, y1, y2, c):
@@ -35,9 +36,9 @@ def mensuration_by_parts2(f, ax1, ax2, start, finish, step=0.01):
     return s
 
 
-start = 1.0
+start = 4.0
 finish = 5.0
-step = 0.01
+step = 0.0001
 x = np.arange(start, finish, step)
 y = function(x)
 fig, ax = plt.subplots(2, 2)
@@ -45,8 +46,8 @@ for i in range(2):
     for j in range(2):
         ax[i, j].plot(x, y, color='black')
 fig.delaxes(ax[0, 1])
-s1 = mensuration_by_parts1(function, 1, 0, start, finish, step=0.5)
-s2 = mensuration_by_parts2(function, 1, 1, start, finish, step=0.5)
+s1 = mensuration_by_parts1(function, 1, 0, start, finish, step=step)
+s2 = mensuration_by_parts2(function, 1, 1, start, finish, step=step)
 diff = abs(s2 - s1)
 print(f's1 = {s1}, s2 = {s2}, diff = {diff}')
 plt.show()
